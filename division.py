f4d55aae0eba4e2a60b5cd90764ddae93ddaba5c
TommyMart <15423@coderacademy.edu.au>
2024-04-11 20:20:26 +0930

    add division

---
+++ division.py
@@ -0,0 +1,20 @@
+# input numerator as n and denominator as d
+
+n = int(input("Enter numerator: "))
+d = int(input("Enter denominator: "))
+
+# if d == 0 
+if d == 0:
+    # display "denominator cannot be zero"
+    print("Denominator cannot be zero")
+
+# else d != 0
+else:
+    # q = n / d
+    q = n / d
+    # display q
+    print(q)
+    
+
+
+
